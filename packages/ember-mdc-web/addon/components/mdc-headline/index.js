5e1bf4865689944316b38d80f8c21442c68802e1
Vish Desai <shadyvd@hotmail.com>
2022-02-08 13:44:33 +0530

    feat(init): commit of the ember-mdc-web package/addon
    First commit of the ember-mdc-web addon... with just the basic styling, empty components, etc.
    
    Signed-off-by:Vish Desai <shadyvd@hotmail.com>

---
+++ packages/ember-mdc-web/addon/components/mdc-headline/index.js
@@ -0,0 +1,36 @@
+import Component from '@glimmer/component';
+import debugLogger from 'ember-debug-logger';
+
+export default class MdcHeadlineComponent extends Component {
+	// #region Accessed Services
+	// #endregion
+
+	// #region Tracked Attributes
+	// #endregion
+
+	// #region Constructor
+	constructor() {
+		super(...arguments);
+		this.#debug(`constructor`);
+	}
+	// #endregion
+
+	// #region Lifecycle Hooks
+	// #endregion
+
+	// #region DOM Event Handlers
+	// #endregion
+
+	// #region Computed Properties
+	// #endregion
+
+	// #region Private Methods
+	// #endregion
+
+	// #region Default Sub-components
+	// #endregion
+
+	// #region Private Attributes
+	#debug = debugLogger('component:mdc-headline');
+	// #endregion
+}
