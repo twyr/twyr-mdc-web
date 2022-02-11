9dc591036ff401dcaf2c30ba488dd4a1940e99c0
Vish Desai <shadyvd@hotmail.com>
2022-02-11 19:31:23 +0530

    style(test-app): nothing much here, for now at least
    Signed-off-by:Vish Desai <shadyvd@hotmail.com>

--- packages/test-app/config/environment.js
+++ packages/test-app/config/environment.js
@@ -23,7 +23,7 @@ module.exports = function (environment) {
 			// when it is created
 		},
 
-		googleFonts: ['Roboto', 'Noto Sans']
+		googleFonts: ['Inter', 'Josefin Sans']
 	};
 
 	if (environment === 'development') {
--- packages/test-app/ember-cli-build.js
+++ packages/test-app/ember-cli-build.js
@@ -5,9 +5,6 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function (defaults) {
 	let app = new EmberApp(defaults, {
 		// Add options here
-		'ember-freestyle': {
-			includeStyles: false
-		},
 		sassOptions: {
 			implementation: require('sass'),
 			includePaths: ['node_modules']
