9e1a5d95bcdff722fd1b1da9a2f8e175c3a5a037
Vish Desai <shadyvd@hotmail.com>
2022-02-13 19:38:42 +0530

    chore(ember-cli-build): import @mdi/font to provide icons
    Signed-off-by:Vish Desai <shadyvd@hotmail.com>

--- packages/test-app/ember-cli-build.js
+++ packages/test-app/ember-cli-build.js
@@ -5,6 +5,8 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 module.exports = function (defaults) {
 	let app = new EmberApp(defaults, {
 		// Add options here
+		storeConfigInMeta: false,
+
 		sassOptions: {
 			implementation: require('sass'),
 			includePaths: ['node_modules']
@@ -23,6 +25,9 @@ module.exports = function (defaults) {
 	// modules that you would like to import into your application
 	// please specify an object with the list of modules as keys
 	// along with the exports of each module as its value.
+	app.import(
+		'node_modules/@mdi/font/fonts/materialdesignicons-webfont.woff2'
+	);
 
 	const { Webpack } = require('@embroider/webpack');
 	return require('@embroider/compat').compatBuild(app, Webpack, {
