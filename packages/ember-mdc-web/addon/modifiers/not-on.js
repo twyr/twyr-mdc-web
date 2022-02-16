415db548115a1cdcea8c92d35c288794c9ae9bdf
Vish Desai <shadyvd@hotmail.com>
2022-02-16 16:58:43 +0530

    wip(modifier): not-on modifier
    Capture events (like clicks, for example) that happen outside the element - and react to them
    
    Signed-off-by:Vish Desai <shadyvd@hotmail.com>

---
+++ packages/ember-mdc-web/addon/modifiers/not-on.js
@@ -0,0 +1,84 @@
+import Modifier from 'ember-modifier';
+import debugLogger from 'ember-debug-logger';
+
+import { supportsPassiveEventListeners } from './../utils/check-browser-features';
+
+export default class NotOnModifier extends Modifier {
+	// #region Accessed Services
+	// #endregion
+
+	// #region Constructor
+	constructor() {
+		super(...arguments);
+		this.#debug(`constructor`);
+	}
+	// #endregion
+
+	// #region Lifecycle Hooks
+	didReceiveArguments() {
+		super.didReceiveArguments(...arguments);
+		this.#debug(
+			`didReceiveArguments:\nelement: `,
+			this?.element,
+			`\nargs: `,
+			this?.args
+		);
+
+		// eslint-disable-next-line curly
+		if (this.#event && this.#eventHandler) {
+			document.removeEventListener(
+				this.#event,
+				this?._eventHandler?.bind?.(this),
+				this.#defaultOptions
+			);
+		}
+
+		this.#event = this?.args?.positional?.[0];
+		this.#eventHandler = this?.args?.positional?.[1];
+
+		document.addEventListener(
+			this.#event,
+			this?._eventHandler?.bind?.(this),
+			this.#defaultOptions
+		);
+	}
+	// #endregion
+
+	// #region DOM Event Handlers
+	// #endregion
+
+	// #region Computed Properties
+	// #endregion
+
+	// #region Private Methods
+	_eventHandler(event) {
+		const isEventOutsideElement =
+			event.target !== this.element &&
+			!this.element.contains(event.target);
+		this.#debug(
+			`_eventHandler:\nelement: `,
+			this?.element,
+			`\nevent: `,
+			this.#event,
+			`\noutside? `,
+			isEventOutsideElement
+		);
+
+		if (!isEventOutsideElement) return;
+
+		this.#eventHandler?.(event);
+	}
+	// #endregion
+
+	// #region Private Attributes
+	#debug = debugLogger('modifier:not-on');
+
+	#defaultOptions = {
+		capture: false,
+		passive: supportsPassiveEventListeners
+	};
+
+	#event = null;
+	#eventHandler = null;
+	// #endregion
+}
