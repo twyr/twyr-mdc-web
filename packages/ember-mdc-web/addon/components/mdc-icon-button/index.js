a3089895ae950c0bd080863793d33028b9d105ca
Vish Desai <shadyvd@hotmail.com>
2022-02-16 23:53:52 +0530

    feat(mdc-icon-button): component with only an icon on it
    Signed-off-by:Vish Desai <shadyvd@hotmail.com>

---
+++ packages/ember-mdc-web/addon/components/mdc-icon-button/index.js
@@ -0,0 +1,62 @@
+import Component from '@glimmer/component';
+import debugLogger from 'ember-debug-logger';
+
+import { action } from '@ember/object';
+import { MDCRipple } from '@material/ripple/index';
+
+export default class MdcIconButtonComponent extends Component {
+	// #region Accessed Services
+	// #endregion
+
+	// #region Tracked Attributes
+	// #endregion
+
+	// #region Constructor
+	constructor() {
+		super(...arguments);
+		this.#debug?.(`constructor`);
+	}
+	// #endregion
+
+	// #region Lifecycle Hooks
+	// #endregion
+
+	// #region DOM Event Handlers
+	@action
+	recalcStyling() {
+		this.#debug?.(`recalcStyling: re-calculating styling`);
+		if (!this.#element) return;
+
+		const paletteColour = `--mdc-theme-${this?.args?.palette ?? 'primary'}`;
+
+		this.#element.style.color = `var(${paletteColour})`;
+		this.#element?.style?.setProperty?.(
+			'--mdc-ripple-color',
+			`var(${paletteColour})`
+		);
+	}
+
+	@action
+	storeElement(element) {
+		this.#debug?.(`storeElement: `, element);
+		this.#element = element;
+
+		this?.recalcStyling?.();
+		MDCRipple?.attachTo?.(this.#element);
+	}
+	// #endregion
+
+	// #region Computed Properties
+	// #endregion
+
+	// #region Private Methods
+	// #endregion
+
+	// #region Default Sub-components
+	// #endregion
+
+	// #region Private Attributes
+	#debug = debugLogger?.('component:mdc-icon-button');
+	#element = null;
+	// #endregion
+}
