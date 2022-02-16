064116d502c4d927bd7de38e1274f4a75e8fca94
Vish Desai <shadyvd@hotmail.com>
2022-02-16 11:33:51 +0530

    feat(mdc-button): component with style, shape, and color
    Button component with 4 styles, 2 shapes, and 3 color palettes
    
    Signed-off-by:Vish Desai <shadyvd@hotmail.com>

--- packages/ember-mdc-web/addon/components/mdc-button/index.js
+++ packages/ember-mdc-web/addon/components/mdc-button/index.js
@@ -51,40 +51,75 @@ export default class MdcButtonComponent extends Component {
 			'--mdc-outlined-button-hover-state-layer-color'
 		);
 
-		// Step 2: Style for each type of button
-		const paletteColor = `--mdc-theme-${this?.args?.palette ?? 'primary'}`;
+		// Raised (protected) button
+		this.#element?.style?.removeProperty?.(
+			'--mdc-protected-button-label-text-color'
+		);
+		this.#element?.style?.removeProperty?.(
+			'--mdc-protected-button-container-color'
+		);
 
+		// Filled (Un-elevated) button
+		this.#element?.style?.removeProperty?.(
+			'--mdc-filled-button-label-text-color'
+		);
+		this.#element?.style?.removeProperty?.(
+			'--mdc-filled-button-container-color'
+		);
+
+		// Step 2: Style / Palette for each button type
+		const paletteColour = `--mdc-theme-${this?.args?.palette ?? 'primary'}`;
+		const textColour = `--mdc-theme-on-${this?.args?.palette ?? 'primary'}`;
+
+		// Non-styled (regular) button
 		this.#element?.style?.setProperty?.(
 			'--mdc-text-button-label-text-color',
-			`var(${paletteColor})`
+			`var(${paletteColour})`
 		);
 		this.#element?.style?.setProperty?.(
 			'--mdc-text-button-hover-state-layer-color',
-			`var(${paletteColor})`
+			`var(${paletteColour})`
 		);
 
 		// Outlined button
-		if (this?.args?.outline) {
-			this.#element?.style?.setProperty?.(
-				'--mdc-text-button-label-text-color',
-				`var(${paletteColor})`
-			);
-			this.#element?.style?.setProperty?.(
-				'--mdc-text-button-hover-state-layer-color',
-				`var(${paletteColor})`
-			);
+		if (this?.args?.style === 'outlined') {
 			this.#element?.style?.setProperty?.(
 				'--mdc-outlined-button-outline-color',
-				`var(${paletteColor})`
+				`var(${paletteColour})`
 			);
 
 			this.#element?.style?.setProperty?.(
 				'--mdc-outlined-button-label-text-color',
-				`var(${paletteColor})`
+				`var(${paletteColour})`
 			);
+
 			this.#element?.style?.setProperty?.(
 				'--mdc-outlined-button-hover-state-layer-color',
-				`var(${paletteColor})`
+				`var(${paletteColour})`
+			);
+		}
+
+		// Raised button
+		if (this?.args?.style === 'raised') {
+			this.#element?.style?.setProperty?.(
+				'--mdc-protected-button-label-text-color',
+				`var(${textColour})`
+			);
+			this.#element?.style?.setProperty?.(
+				'--mdc-protected-button-container-color',
+				`var(${paletteColour})`
+			);
+		}
+
+		// Flat button
+		if (this?.args?.style === 'flat') {
+			this.#element?.style?.setProperty?.(
+				'--mdc-filled-button-label-text-color',
+				`var(${textColour})`
+			);
+			this.#element?.style?.setProperty?.(
+				'--mdc-filled-button-container-color',
+				`var(${paletteColour})`
 			);
 		}
 	}
