d69a7d628050285dfa9d9ceb8ce60a9c0a826135
Vish Desai <shadyvd@hotmail.com>
2022-02-17 18:01:16 +0530

    feat(mdc-fab): floating action button
    Floatng button with just an icon, or an icon with text
    
    Signed-off-by:Vish Desai <shadyvd@hotmail.com>

---
+++ packages/ember-mdc-web/addon/components/mdc-fab/index.js
@@ -0,0 +1,72 @@
+import Component from '@glimmer/component';
+import debugLogger from 'ember-debug-logger';
+
+import { action } from '@ember/object';
+import { MDCRipple } from '@material/ripple/index';
+
+export default class MdcFabComponent extends Component {
+	// #region Accessed Services
+	// #endregion
+
+	// #region Tracked Attributes
+	// #endregion
+
+	// #region Constructor
+	constructor() {
+		super(...arguments);
+		this.#debug?.(`constructor`);
+	}
+	// #endregion
+
+	// #region Lifecycle Hooks
+	// #endregion
+
+	// #region DOM Event Handlers
+	@action
+	recalcStyling() {
+		this.#debug?.(`recalcStyling: re-calculating styling`);
+		if (!this.#element) return;
+
+		const paletteColour = `--mdc-theme-${
+			this?.args?.palette ?? 'secondary'
+		}`;
+		const textColour = `--mdc-theme-on-${
+			this?.args?.palette ?? 'secondary'
+		}`;
+
+		this.#element.style.backgroundColor = `var(${paletteColour})`;
+		this.#element.style.color = `var(${textColour})`;
+
+		const iconElement = this.#element?.querySelector?.('i.mdc-fab__icon');
+		iconElement.style.color = `var(${textColour})`;
+
+		this.#element?.style?.setProperty?.(
+			'--mdc-ripple-color',
+			`var(${textColour})`
+		);
+	}
+
+	@action
+	storeElement(element) {
+		this.#debug?.(`storeElement: `, element);
+		this.#element = element;
+
+		this?.recalcStyling?.();
+		MDCRipple?.attachTo?.(this.#element);
+	}
+	// #endregion
+
+	// #region Computed Properties
+	// #endregion
+
+	// #region Private Methods
+	// #endregion
+
+	// #region Default Sub-components
+	// #endregion
+
+	// #region Private Attributes
+	#debug = debugLogger?.('component:mdc-fab');
+	#element = null;
+	// #endregion
+}
