a16ab99ad6eb83d27e9ce9eb43a0ba7ac57c6a84
Vish Desai <shadyvd@hotmail.com>
2022-02-17 19:31:23 +0530

    test(test-app): random code to test ember-mdc-components
    Signed-off-by:Vish Desai <shadyvd@hotmail.com>

--- packages/test-app/app/controllers/application.js
+++ packages/test-app/app/controllers/application.js
@@ -1,3 +1,42 @@
 import Controller from '@ember/controller';
+import debugLogger from 'ember-debug-logger';
 
-export default class ApplicationController extends Controller {}
+import { action } from '@ember/object';
+
+export default class ApplicationController extends Controller {
+	// #region Accessed Services
+	// #endregion
+
+	// #region Tracked Attributes
+	// #endregion
+
+	// #region Constructor
+	constructor() {
+		super(...arguments);
+		this.#debug?.(`constructor`);
+	}
+	// #endregion
+
+	// #region Lifecycle Hooks
+	// #endregion
+
+	// #region DOM Event Handlers
+	@action
+	showDialog() {
+		this.#debug?.('showDialog: ON BUTTON CLICK');
+	}
+	// #endregion
+
+	// #region Computed Properties
+	// #endregion
+
+	// #region Private Methods
+	// #endregion
+
+	// #region Default Sub-components
+	// #endregion
+
+	// #region Private Attributes
+	#debug = debugLogger?.('application:test-app');
+	// #endregion
+}
