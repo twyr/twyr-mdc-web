6eb5003edb6dfdb2d7ea8f4a544a6072f5326116
Vish Desai <shadyvd@hotmail.com>
2022-02-22 17:26:31 +0530

    wip(test-app): use list-group in the sidebar
    Signed-off-by:Vish Desai <shadyvd@hotmail.com>

--- packages/test-app/app/controllers/application.js
+++ packages/test-app/app/controllers/application.js
@@ -10,7 +10,7 @@ export default class ApplicationController extends Controller {
 
 	// #region Tracked Attributes
 	@tracked navIconElement = null;
-	@tracked palette = 'secondary';
+	@tracked palette = false;
 	// #endregion
 
 	// #region Constructor
