32ba6111ee021e41685b9256acebedcedfff5fdd
Vish Desai <shadyvd@hotmail.com>
2022-03-14 17:39:26 +0530

    feat(mdc-tab): component to show multiple headings in the same row
    Signed-off-by:Vish Desai <shadyvd@hotmail.com>

--- packages/ember-mdc-web/addon/components/mdc-tab-bar/index.js
+++ packages/ember-mdc-web/addon/components/mdc-tab-bar/index.js
@@ -10,6 +10,9 @@ export default class MdcTabBarComponent extends Component {
 
 	// #region Tracked Attributes
 	@tracked scrollEnabled = false;
+	@tracked rightScrollDisabled = false;
+	@tracked leftScrollDisabled = false;
+
 	controls = {};
 	// #endregion
 
@@ -34,11 +37,43 @@ export default class MdcTabBarComponent extends Component {
 
 	// #region DOM Event Handlers
 	@action
+	shouldEnableScroll() {
+		this.#debug?.(`shouldEnableScroll`);
+		if (!this.#element) return;
+
+		const scrollAreaDimensions = this.#element
+			?.querySelector?.('div.mdc-tab-scroller__scroll-area')
+			?.getBoundingClientRect?.();
+
+		const scrollContentDimensions = this.#element
+			?.querySelector?.('div.mdc-tab-scroller__scroll-content')
+			?.getBoundingClientRect?.();
+
+		const scrollEnabled =
+			scrollAreaDimensions?.width < scrollContentDimensions?.width;
+		if (scrollEnabled === this.scrollEnabled) return;
+
+		this.#debug?.(
+			`shouldEnableScroll:\nScroll Area Width ${scrollAreaDimensions?.width}\nScroll Content Width ${scrollContentDimensions?.width}`
+		);
+
+		this.scrollEnabled = scrollEnabled;
+		this.#debug?.(`shouldEnableScroll: ${this.scrollEnabled}`);
+	}
+
+	@action
+	handleScroll(direction) {
+		this.#debug?.(`handleScroll: ${direction}`);
+
+		if (direction === 'left') this?._scrollLeft?.();
+		else this?._scrollRight?.();
+	}
+
+	@action
 	storeElement(element) {
 		this.#debug?.(`storeElement: `, element);
 		this.#element = element;
 
-		this?._shouldEmableScroll?.();
 		this?._fireEvent?.('init');
 	}
 	// #endregion
@@ -59,8 +94,6 @@ export default class MdcTabBarComponent extends Component {
 		} else {
 			this.#items?.set?.(item, controls);
 		}
-
-		this?._shouldEmableScroll?.();
 	}
 
 	@action
@@ -92,29 +125,6 @@ export default class MdcTabBarComponent extends Component {
 	}
 
 	@action
-	_shouldEmableScroll() {
-		this.#debug?.(`_shouldEmableScroll`);
-		if (!this.#element) return;
-
-		const scrollAreaDimensions = this.#element
-			?.querySelector?.('div.mdc-tab-scroller__scroll-area')
-			?.getBoundingClientRect?.();
-		const scrollContentDimensions = this.#element
-			?.querySelector?.('div.mdc-tab-scroller__scroll-content')
-			?.getBoundingClientRect?.();
-
-		this.#debug?.(
-			`_shouldEmableScroll:\nScroll Area Width ${scrollAreaDimensions?.width}\nScroll Content Width ${scrollContentDimensions?.width}`
-		);
-		if (scrollAreaDimensions?.width >= scrollContentDimensions?.width) {
-			this.#debug?.(`_shouldEmableScroll: No need for scrolling`);
-			return;
-		}
-
-		this.#debug?.(`_shouldEmableScroll: ENABLE: TAB SCROLL`);
-	}
-
-	@action
 	_fireEvent(name, options) {
 		this.#debug?.(`_fireEvent`);
 		if (!this.#element) return;
@@ -139,6 +149,54 @@ export default class MdcTabBarComponent extends Component {
 		});
 
 		this.#element?.dispatchEvent?.(thisEvent);
+	}
+
+	_scrollLeft() {
+		const scrollAreaElement = this.#element?.querySelector?.(
+			'div.mdc-tab-scroller__scroll-area'
+		);
+		const scrollAreaRect = scrollAreaElement?.getBoundingClientRect?.();
+
+		let lastInvisibleTabRect = null;
+
+		const tabs = scrollAreaElement?.querySelectorAll?.('button.mdc-tab');
+		for (let idx = 0; idx < tabs?.length; idx++) {
+			const thisTab = tabs[idx];
+			const tabRect = thisTab?.getBoundingClientRect?.();
+
+			if (tabRect?.left <= scrollAreaRect?.left) {
+				lastInvisibleTabRect = tabRect;
+				continue;
+			}
+
+			break;
+		}
+
+		if (!lastInvisibleTabRect) return;
+		scrollAreaElement.scrollLeft -= lastInvisibleTabRect.width;
+	}
+
+	_scrollRight() {
+		const scrollAreaElement = this.#element?.querySelector?.(
+			'div.mdc-tab-scroller__scroll-area'
+		);
+		const scrollAreaRect = scrollAreaElement?.getBoundingClientRect?.();
+
+		let firstInvisibleTabRect = null;
+
+		const tabs = scrollAreaElement?.querySelectorAll?.('button.mdc-tab');
+		for (let idx = 0; idx < tabs?.length; idx++) {
+			const thisTab = tabs[idx];
+			const tabRect = thisTab?.getBoundingClientRect?.();
+
+			if (tabRect?.right <= scrollAreaRect?.right) continue;
+
+			firstInvisibleTabRect = tabRect;
+			break;
+		}
+
+		if (!firstInvisibleTabRect) return;
+		scrollAreaElement.scrollLeft += firstInvisibleTabRect.width;
 	}
 
 	_getComputedSubcomponent(componentName) {
