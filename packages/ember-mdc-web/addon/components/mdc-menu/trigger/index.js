d0a6f02532b91d7f498e434f2928db46e295d8f3
Vish Desai <shadyvd@hotmail.com>
2022-03-31 14:11:42 +0530

    fix(mdc-menu): palette sensitivity for the trigger
    Signed-off-by:Vish Desai <shadyvd@hotmail.com>

--- packages/ember-mdc-web/addon/components/mdc-menu/trigger/index.js
+++ packages/ember-mdc-web/addon/components/mdc-menu/trigger/index.js
@@ -52,11 +52,56 @@ export default class MdcMenuTriggerComponent extends Component {
 
 	// #region Modifier Callbacks
 	@action
+	recalcStyles() {
+		this.#debug?.(`recalcStyles`);
+		if (!this.#element) return;
+
+		const textElement = this.#element?.querySelector?.(
+			'span.mdc-list-item__text'
+		);
+
+		const primaryTextElement = this.#element?.querySelector?.(
+			'span.mdc-list-item__primary-text'
+		);
+
+		// Step 1: Reset
+		// TODO: Optimize this by unsetting only those properties that have not been utilitized
+		// in the current scenario
+		this.#element?.style?.removeProperty?.('--mdc-ripple-color');
+
+		this.#element.style.borderRadius = null;
+		if (textElement) textElement.style.color = null;
+		if (primaryTextElement) primaryTextElement.style.color = null;
+
+		// Stop if the element is disabled
+		if (this.#element?.hasAttribute?.('disabled')) return;
+
+		// Step 2: Style / Palette
+		if (this?.args?.shaped) {
+			this.#element.style.borderRadius = '0 2rem 2rem 0';
+		}
+
+		if (this?.args?.palette) {
+			this.#element?.style?.setProperty?.(
+				'--mdc-ripple-color',
+				`var(--mdc-theme-${this?.args?.palette})`
+			);
+
+			if (textElement)
+				textElement.style.color = `var(--mdc-theme-${this?.args?.palette})`;
+
+			if (primaryTextElement)
+				primaryTextElement.style.color = `var(--mdc-theme-${this?.args?.palette})`;
+		}
+	}
+
+	@action
 	storeElement(element) {
 		this.#debug(`storeElement: `, element);
 		super.storeElement?.(element);
 
 		this.#element = element;
+		this?.recalcStyles?.();
 	}
 	// #endregion
 
