ede7bc9ff3d85760d19246b8888858f00a44a808
Vish Desai <shadyvd@hotmail.com>
2022-04-07 16:17:37 +0530

    chore(prettier): <pre> tag formatting
    Disable prettier formattign inside the <pre> tag - what's the point?
    
    Signed-off-by:Vish Desai <shadyvd@hotmail.com>

--- packages/ember-mdc-web/.template-lintrc.js
+++ packages/ember-mdc-web/.template-lintrc.js
@@ -3,9 +3,5 @@
 module.exports = {
 	plugins: ['ember-template-lint-plugin-prettier'],
 
-	extends: ['recommended', 'ember-template-lint-plugin-prettier:recommended'],
-
-	rules: {
-		'self-closing-void-elements': true
-	}
+	extends: ['recommended', 'ember-template-lint-plugin-prettier:recommended']
 };
--- packages/test-app/.template-lintrc.js
+++ packages/test-app/.template-lintrc.js
@@ -3,15 +3,5 @@
 module.exports = {
 	plugins: ['ember-template-lint-plugin-prettier'],
 
-	extends: ['recommended', 'ember-template-lint-plugin-prettier:recommended'],
-
-	ignore: [
-		'twyr-mdc-web/packages/test-app/app/templates/first-steps.hbs',
-		'test-app/app/templates/first-steps.hbs',
-		'app/templates/first-steps.hbs'
-	],
-
-	rules: {
-		'self-closing-void-elements': true
-	}
+	extends: ['recommended', 'ember-template-lint-plugin-prettier:recommended']
 };
